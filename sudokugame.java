7a4012c4b166a61b4cc356be9250b0619531b4bb
HARSHg2112 <harshgupta_cs21@gla.ac.in>
2024-07-11 23:58:19 +0530

    last commit

---
+++ sudokugame.java
@@ -0,0 +1,77 @@
+public class sudokugame {
+    private static final int SIZE = 9;
+    private static final int EMPTY = 0;
+
+    public boolean solveSudoku(int[][] board) {
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                if (board[row][col] == EMPTY) {
+                    for (int number = 1; number <= SIZE; number++) {
+                        if (isValid(board, row, col, number)) {
+                            board[row][col] = number;
+                            if (solveSudoku(board)) {
+                                return true;
+                            } else {
+                                board[row][col] = EMPTY;
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean isValid(int[][] board, int row, int col, int number) {
+        for (int i = 0; i < SIZE; i++) {
+            if (board[row][i] == number || board[i][col] == number) {
+                return false;
+            }
+        }
+
+        int boxRowStart = row - row % 3;
+        int boxColStart = col - col % 3;
+        for (int r = boxRowStart; r < boxRowStart + 3; r++) {
+            for (int d = boxColStart; d < boxColStart + 3; d++) {
+                if (board[r][d] == number) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void printBoard(int[][] board) {
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                System.out.print(board[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] board = {
+            {5, 3, 0, 0, 7, 0, 0, 0, 0},
+            {6, 0, 0, 1, 9, 5, 0, 0, 0},
+            {0, 9, 8, 0, 0, 0, 0, 6, 0},
+            {8, 0, 0, 0, 6, 0, 0, 0, 3},
+            {4, 0, 0, 8, 0, 3, 0, 0, 1},
+            {7, 0, 0, 0, 2, 0, 0, 0, 6},
+            {0, 6, 0, 0, 0, 0, 2, 8, 0},
+            {0, 0, 0, 4, 1, 9, 0, 0, 5},
+            {0, 0, 0, 0, 8, 0, 0, 7, 9}
+        };
+
+        sudokugame sudokuGame = new sudokugame();
+        if (sudokuGame.solveSudoku(board)) {
+            sudokuGame.printBoard(board);
+        } else {
+            System.out.println("No solution exists.");
+        }
+    }
+}
+
+    
+
