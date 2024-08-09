5938bc2309b9d9f39194c74076c3e4ec1ff6a6c5
HARSHg2112 <harshgupta_cs21@gla.ac.in>
2024-08-09 19:01:29 +0530

    lastcommit

--- subarraysumzero.java
+++ subarraysumzero.java
@@ -1,28 +1,20 @@
+import java.util.*;
 public class subarraysumzero {
-
-    public static void findSubarrays(int[] arr) {
-        int n = arr.length;
-
-        for (int i = 0; i < n; i++) {
-            int sum = 0;
-            for (int j = i; j < n; j++) {
-                sum += arr[j];
-                if (sum == 0) {
-                    printSubarray(arr, i, j);
-                }
+    public static void main(String args[]){
+        int arr[]={15,-2,2,-8,1,7,10,23};
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int sum=0;
+        int length=0;
+        for(int j=0;j<arr.length;j++){
+            sum=sum+arr[j];
+            if(map.containsKey(sum)){
+                length=Math.max(length,j-map.get(sum));
+            }
+            else{
+                map.put(sum,j);
             }
         }
-    }
+        System.out.println(length);
 
-    private static void printSubarray(int[] arr, int start, int end) {
-        for (int i = start; i <= end; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-    }
-
-    public static void main(String[] args) {
-        int[] arr = {3, 4, -7, 1, 2, -1, 1, -2, -3};
-        findSubarrays(arr);
     }
 }
